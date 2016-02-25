e1e6b43a80921e8be8a6b6c7da61b61e6ebbf2d2
79612830649 <79612830649@lab307.est.udicentro.local>
2016-02-24 22:30:34 -0300

    ok

--- SistemaEcommerce/src/model/faced/ws/ClienteFacade.java
+++ SistemaEcommerce/src/model/faced/ws/ClienteFacade.java
@@ -18,4 +18,18 @@ public List<Cliente> getCliente(){
 	return Arrays.asList(new Cliente(1, "Marco Antonio","marcoantoniosisnet@hotmail.com"),
 			new Cliente (2, "Eliseu ","eliseumcd@gmail.com"));
 	}
+	
+	@WebMethod(operationName="getClienteParametro")
+	public List<Cliente>getCliente(Cliente cliente){
+		if (cliente.getCodigo() ==1){
+			return Arrays.asList(new Cliente(1, "Izabela quer parametro","izabela@gmail.com"));
+		
+		}else{
+	return Arrays.asList(new Cliente(1, "Izabela nao quer parametro","izabela@gmail.com"));
+		
+		}
+		
+	}
 }
+
+
