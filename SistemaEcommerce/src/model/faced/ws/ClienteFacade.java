eafe19bd24772a857d9b70b004ca2f666089184d
MARCO ANTONIO FORTUNATO <79612830649@est.udicentro.local>
2016-02-24 21:23:31 -0300

    commit inicial

---
+++ SistemaEcommerce/src/model/faced/ws/ClienteFacade.java
@@ -0,0 +1,20 @@
+package model.faced.ws;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import java.util.Arrays;
+import java.util.List;
+
+import model.domain.Cliente;
+
+
+@WebService(serviceName="ws/cliente")
+public class ClienteFacade {
+	
+	@WebMethod
+public List<Cliente> getCliente(){
+		
+	
+	return Arrays.asList(new Cliente(1, "Marco Antonio","marcoantoniosisnet@hotmail.com"));
+	}
+}
